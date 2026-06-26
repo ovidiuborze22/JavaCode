89da4227f2365d70293430e52874bc49b2417256
Ovidiu Borze <ovidiuborze22@gmail.com>
2026-06-26 23:01:53 +0100

    Successful attempt to save data

--- HibernateDemo/src/main/java/org/example/Main.java
+++ HibernateDemo/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
@@ -11,11 +12,16 @@ public class Main {
         s1.setRollNo(101);
         s1.setsName("John");
         s1.setsAge(30);
-        Configuration cfg=new Configuration();
-        SessionFactory sf=cfg.buildSessionFactory();
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(org.example.Student.class);
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+
         session.persist(s1);
 
+        transaction.commit();
         System.out.println(s1);
     }
 
