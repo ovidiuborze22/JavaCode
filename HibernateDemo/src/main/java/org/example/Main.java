a5ba8f82190d091be5cd0c5395db9e5141e7709e
Ovidiu Borze <ovidiuborze22@gmail.com>
2026-09-14 23:19:39 +0100

    Changing table and column names

--- HibernateDemo/src/main/java/org/example/Main.java
+++ HibernateDemo/src/main/java/org/example/Main.java
@@ -1,40 +1,28 @@
 package org.example;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-
-
-//        s1.setRollNo(103);
-//        s1.setsName("Harsh");
-//        s1.setsAge(24);
-
-        s1.setRollNo(107);
-        s1.setsName("Arthur");
-        s1.setsAge(29);
-
-//        Student s2 = null;
+        Alien a1 = new Alien();
+        a1.setAid(101);
+        a1.setAname("John");
+        a1.setTech("Java");
 
         SessionFactory sf = new Configuration()
                 .configure()
-                .addAnnotatedClass(org.example.Student.class)
+                .addAnnotatedClass(org.example.Alien.class)
                 .buildSessionFactory();
 
         Session session = sf.openSession();
-        s1 = session.find(Student.class, 107);
+
         Transaction transaction = session.beginTransaction();
-//        session.merge(s1); // update
-        session.remove(s1); // remove
+        session.persist(a1);
         transaction.commit();
         session.close();
         sf.close();
-        System.out.println(s1);
 
     }
 }
