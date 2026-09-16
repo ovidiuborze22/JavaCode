8763b9b7eb3ff63b16e5b5a2d2d10f63f54264eb
Ovidiu Borze <ovidiuborze22@gmail.com>
2026-09-16 20:38:44 +0100

    Fetching with filter and specific properties

--- HibernateDemo/src/main/java/org/example/Main.java
+++ HibernateDemo/src/main/java/org/example/Main.java
@@ -19,12 +19,22 @@ public class Main {
 //   Select * from laptop where ram=32 ->SQL
 //        from Laptop where ram=32 -> HQL
 
+        String brand = "Asus";
 
-        Query query = session.createQuery("from Laptop where ram=16",Laptop.class);
-        List<Laptop> laptops = query.getResultList();
+//        Query query = session.createQuery("from Laptop where brand like 'Asus' ");
 
+        Query query = session.createQuery("select brand, model from Laptop where brand like ?1",Laptop.class);
+        query.setParameter(1, brand);
 
+//        List<Laptop> laptops = query.getResultList();
+        List<Object[]> laptops = query.getResultList();
+
+        for (Object[] data : laptops) {
+            System.out.println((String) data[0] + " : " + (String) data[1]);
+        }
 //        Laptop l1=session.get(Laptop.class, 3);
+
+
         System.out.println(laptops);
         session.close();
 
